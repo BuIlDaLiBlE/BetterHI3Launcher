073f3d89823fa852336c31e1e1339959382b9595
neon-nyan <dev.kemalsetyaa@gmail.com>
2022-05-17 19:05:41 +0700

    Delay status refresh to 250ms each

--- Utility/DownloadProgressEvents.cs
+++ Utility/DownloadProgressEvents.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Shell;
 using BetterHI3Launcher.Utility;
@@ -8,51 +9,61 @@ namespace BetterHI3Launcher
 {
 	public partial class MainWindow : Window
 	{
+		float RefreshRate = 250f;
+		Stopwatch LastTimeSpan = Stopwatch.StartNew();
 		private void DownloadStatusChanged(object sender, DownloadChangedProgress e)
 		{
-			Dispatcher.Invoke(() =>
+			if (LastTimeSpan.Elapsed.TotalMilliseconds >= RefreshRate)
 			{
-				double DownloadPercentage = Math.Round(double.IsInfinity(e.ProgressPercentage) ? 0 : e.ProgressPercentage, 2);
-				DownloadProgressBar.Value = DownloadPercentage / 100;
-				TaskbarItemInfo.ProgressValue = DownloadPercentage / 100;
-				DownloadETAText.Text = string.Format(App.TextStrings["progresstext_eta"], string.Format("{0:hh\\:mm\\:ss}", e.TimeLeft));
-				if(e.Status == DownloadState.Merging)
+				Dispatcher.Invoke(() =>
 				{
-					DownloadProgressText.Text = $"{string.Format(App.TextStrings["label_merged"], DownloadPercentage)} ({BpUtility.ToBytesCount(e.BytesReceived)}/{BpUtility.ToBytesCount(e.TotalBytesToReceive)})";
-					DownloadSpeedText.Text = $"{App.TextStrings["label_merge_speed"]} {BpUtility.ToBytesCount(e.CurrentSpeed)}{App.TextStrings["bytes_per_second"].Substring(1)}";
-					DownloadPauseButton.Visibility = Visibility.Collapsed;
-				}
-				else
-				{
-					DownloadProgressText.Text = $"{string.Format(App.TextStrings["label_downloaded_1"], DownloadPercentage)} ({BpUtility.ToBytesCount(e.BytesReceived)}/{BpUtility.ToBytesCount(e.TotalBytesToReceive)})";
-					DownloadSpeedText.Text = $"{App.TextStrings["label_download_speed"]} {BpUtility.ToBytesCount(e.CurrentSpeed)}{App.TextStrings["bytes_per_second"].Substring(1)}";
-				}
-			});
+					double DownloadPercentage = Math.Round(double.IsInfinity(e.ProgressPercentage) ? 0 : e.ProgressPercentage, 2);
+					DownloadProgressBar.Value = DownloadPercentage / 100;
+					TaskbarItemInfo.ProgressValue = DownloadPercentage / 100;
+					DownloadETAText.Text = string.Format(App.TextStrings["progresstext_eta"], string.Format("{0:hh\\:mm\\:ss}", e.TimeLeft));
+					if (e.Status == DownloadState.Merging)
+					{
+						DownloadProgressText.Text = $"{string.Format(App.TextStrings["label_merged"], DownloadPercentage)} ({BpUtility.ToBytesCount(e.BytesReceived)}/{BpUtility.ToBytesCount(e.TotalBytesToReceive)})";
+						DownloadSpeedText.Text = $"{App.TextStrings["label_merge_speed"]} {BpUtility.ToBytesCount(e.CurrentSpeed)}{App.TextStrings["bytes_per_second"].Substring(1)}";
+						DownloadPauseButton.Visibility = Visibility.Collapsed;
+					}
+					else
+					{
+						DownloadProgressText.Text = $"{string.Format(App.TextStrings["label_downloaded_1"], DownloadPercentage)} ({BpUtility.ToBytesCount(e.BytesReceived)}/{BpUtility.ToBytesCount(e.TotalBytesToReceive)})";
+						DownloadSpeedText.Text = $"{App.TextStrings["label_download_speed"]} {BpUtility.ToBytesCount(e.CurrentSpeed)}{App.TextStrings["bytes_per_second"].Substring(1)}";
+					}
+				});
+				LastTimeSpan = Stopwatch.StartNew();
+			}
 		}
 		private void PreloadDownloadStatusChanged(object sender, DownloadChangedProgress e)
 		{
-			Dispatcher.Invoke(() =>
+			if (LastTimeSpan.Elapsed.TotalMilliseconds >= RefreshRate)
 			{
-				double DownloadPercentage = Math.Round(double.IsInfinity(e.ProgressPercentage) ? 0 : e.ProgressPercentage, 2);
-				PreloadCircleProgressBar.Value = DownloadPercentage / 100;
-				TaskbarItemInfo.ProgressValue = DownloadPercentage / 100;
-				PreloadStatusTopRightText.Text = $"{BpUtility.ToBytesCount(e.BytesReceived)}/{BpUtility.ToBytesCount(e.TotalBytesToReceive)}";
-				PreloadStatusMiddleRightText.Text = string.Format("{0:hh\\:mm\\:ss}", e.TimeLeft);
-				PreloadStatusBottomRightText.Text = $"{BpUtility.ToBytesCount(e.CurrentSpeed)}{App.TextStrings["bytes_per_second"].Substring(1)}";
-				if(e.Status == DownloadState.Merging)
+				Dispatcher.Invoke(() =>
 				{
-					PreloadPauseButton.IsEnabled = false;
-					PreloadBottomText.Text = string.Format(App.TextStrings["label_merged"], DownloadPercentage);
-					PreloadStatusTopLeftText.Text = $"{App.TextStrings["label_merged"].Split(' ')[0]}:";
-					PreloadStatusBottomLeftText.Text = App.TextStrings["label_merge_speed"];
-				}
-				else
-				{
-					PreloadBottomText.Text = string.Format(App.TextStrings["label_downloaded_1"], DownloadPercentage);
-					PreloadStatusTopLeftText.Text = App.TextStrings["label_downloaded_2"];
-					PreloadStatusBottomLeftText.Text = App.TextStrings["label_download_speed"];
-				}
-			});
+					double DownloadPercentage = Math.Round(double.IsInfinity(e.ProgressPercentage) ? 0 : e.ProgressPercentage, 2);
+					PreloadCircleProgressBar.Value = DownloadPercentage / 100;
+					TaskbarItemInfo.ProgressValue = DownloadPercentage / 100;
+					PreloadStatusTopRightText.Text = $"{BpUtility.ToBytesCount(e.BytesReceived)}/{BpUtility.ToBytesCount(e.TotalBytesToReceive)}";
+					PreloadStatusMiddleRightText.Text = string.Format("{0:hh\\:mm\\:ss}", e.TimeLeft);
+					PreloadStatusBottomRightText.Text = $"{BpUtility.ToBytesCount(e.CurrentSpeed)}{App.TextStrings["bytes_per_second"].Substring(1)}";
+					if (e.Status == DownloadState.Merging)
+					{
+						PreloadPauseButton.IsEnabled = false;
+						PreloadBottomText.Text = string.Format(App.TextStrings["label_merged"], DownloadPercentage);
+						PreloadStatusTopLeftText.Text = $"{App.TextStrings["label_merged"].Split(' ')[0]}:";
+						PreloadStatusBottomLeftText.Text = App.TextStrings["label_merge_speed"];
+					}
+					else
+					{
+						PreloadBottomText.Text = string.Format(App.TextStrings["label_downloaded_1"], DownloadPercentage);
+						PreloadStatusTopLeftText.Text = App.TextStrings["label_downloaded_2"];
+						PreloadStatusBottomLeftText.Text = App.TextStrings["label_download_speed"];
+					}
+				});
+				LastTimeSpan = Stopwatch.StartNew();
+			}
 		}
 	}
 }
