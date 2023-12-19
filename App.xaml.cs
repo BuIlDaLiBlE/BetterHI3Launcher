3117a1579202aa4637a3d88498a6df6ae60ff4b4
BuIlDaLiBlE <a-kurkino@yandex.ru>
2023-12-19 22:06:10 +0300

    Update submodule and nuget packages, bump version

--- App.xaml.cs
+++ App.xaml.cs
@@ -14,7 +14,7 @@ namespace BetterHI3Launcher
 {
 	public partial class App : Application
 	{
-		public static readonly LauncherVersion LocalLauncherVersion = new LauncherVersion("1.5.20231203.0");
+		public static readonly LauncherVersion LocalLauncherVersion = new LauncherVersion("1.5.20231219.0");
 		public static readonly string LauncherRootPath = AppDomain.CurrentDomain.BaseDirectory;
 		public static readonly string LocalLowPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}Low";
 		public static readonly string LauncherDataPath = Path.Combine(LocalLowPath, @"Bp\Better HI3 Launcher");
