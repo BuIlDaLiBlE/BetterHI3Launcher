5943581f619d6efd7162df4c4f50b4a858b9ecbf
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-06-15 19:17:17 +0700

    Remove unused variables + Fix indentation

--- Config/FileMetadata.cs
+++ Config/FileMetadata.cs
@@ -5,8 +5,8 @@ namespace BetterHI3Launcher.Config;
 
 public class FileMetadata
 {
-    public string?        DownloadUrl  { get; set; }
-    public DateTimeOffset ModifiedDate { get; set; }
-    public long           FileSize     { get; set; }
-    public string?        Title        { get; set; }
+	public string?        DownloadUrl  { get; set; }
+	public DateTimeOffset ModifiedDate { get; set; }
+	public long           FileSize     { get; set; }
+	public string?        Title        { get; set; }
 }
